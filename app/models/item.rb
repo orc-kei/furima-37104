01acda40438f5b9dbcaabb68cfe3a4cea6e10e7c
orc-kei <keeeei6228@gmail.com>
2021-11-22 14:34:43 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,28 @@
 class Item < ApplicationRecord
+extend ActiveHash::Associations::ActiveRecordExtensions
+
+ validates :image,            presence: true
  validates :title,            presence: true
  validates :description,      presence: true
- validates :category_id,      presence: true
- validates :condition_id,     presence: true
- validates :delivery_fee_id,  presence: true
- validates :prefecture_id,    presence: true
- validates :delivery_day_id,  presence: true
+ validates :category_id,      numericality: { other_than: 1 , message: "can't be blank" }
+ validates :condition_id,     numericality: { other_than: 1 , message: "can't be blank" }
+ validates :delivery_fee_id,  numericality: { other_than: 1 , message: "can't be blank" }
+ validates :prefecture_id,    numericality: { other_than: 0 , message: "can't be blank" }
+ validates :delivery_day_id,  numericality: { other_than: 1 , message: "can't be blank" }
  validates :price,            presence: true
+ validates :price, numericality: { with: /\A[0-9]+\z/}
+ validates :price,
+           numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'Out of setting range' }
 
  belongs_to :user
  has_one    :buy
-end
+ has_one_attached :image
+
+ 
+ belongs_to :category
+ belongs_to :condition
+ belongs_to :delivery_fee
+ belongs_to :prefecture
+ belongs_to :delivery_day
+
+end 
