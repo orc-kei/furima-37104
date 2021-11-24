f3b9aaed0e95f4d79aa7e0877e869c9f0931d925
orc-kei <keeeei6228@gmail.com>
2021-11-24 17:52:39 +0900

    再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,10 +47,9 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-  if  current_user == @item.user
-    render :edit
-  else
+   unless current_user == @item.user
     redirect_to root_path
+
   end
  end
 end
